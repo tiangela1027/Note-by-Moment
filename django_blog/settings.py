474349fa91871244165d2278a26d3981e5804285
Angela <tiangela1@gmail.com>
2020-07-08 23:30:11 -0700

    Deploying to Heroku.

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for django_blog project.
 
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
